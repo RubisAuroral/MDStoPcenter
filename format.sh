336b0409c0fa0e6d1617af5d0a1d078843280410
RubisAuroral <azanith1610@gmail.com>
2023-03-31 14:08:06 +0200

    Shell to format instance

---
+++ format.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+while read -r line; do
+    for word in $line; do
+        if [[ $word =~ ^[0-9]+$ ]]; then
+            ((word++))
+        fi
+        echo -n "$word "
+    done
+    echo ""
+done < "$1"
