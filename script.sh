5b2188e53d00eff8fd2915dec94faabcf377ce7b
RubisAuroral <83035526+RubisAuroral@users.noreply.github.com>
2023-05-03 15:13:06 +0200

    repare script.sh

--- script.sh
+++ script.sh
@@ -6,12 +6,12 @@ mkdir -p "$dr"
 
 for n in 50 100 200 350
 do
-	for p in 25 50 75
+	for p in 1 5 10
 	do
         for times in A B C D E
         do
-            filename="$dr/${n}_${p}_${times}.txt"
-		    python3 instance_generator.py $n $p > "$filename"
-		done
+            	filename="$dr/${n}_${p}_${times}.txt"
+		python3 instance_generator.py $n $p > "$filename"
+	done
     done
 done
